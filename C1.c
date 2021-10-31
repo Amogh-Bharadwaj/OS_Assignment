59bc5cdb63b0067e8577b39f5c8285c23ce58edb
Amogh-Bharadwaj <amoghrb01@gmail.com>
2021-10-31 18:26:46 +0530

    Corrected to provide sum of numbers.

--- C1.c
+++ C1.c
@@ -11,14 +11,17 @@ int main(int argc, char *argv[]){
     
     //Pass the n1 value as command line argument (like: ./C1 9)
     int n1 = atoi(argv[1]);
+    int sum=0;
     
-    int output[n1];
+    
     printf("Enter %d numbers below:\n", n1);
-    for(int i=0;i<n1;i++)scanf("%d",&output[i]);
+    for(int i=0;i<n1;i++){
+        int x;
+        scanf("%d",&x);
+        sum+=x;
+    }
     
-    printf("Output: \n");
-    for(int i=0;i<n1;i++)printf("%d ",output[i]);
+    printf("Sum: %d\n",sum);
     
-
     return 0;
 }
