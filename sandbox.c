2793bfc93e0ea66af2e9dcd0b57aa345ee802994
Amogh-Bharadwaj <amoghrb01@gmail.com>
2021-11-12 19:19:54 +0530

    Shared memory attempt.

--- sandbox.c
+++ sandbox.c
@@ -1,3 +1,6 @@
+// TO COMPILE: gcc -pthread ThreadTesting.c -o sandbox. 
+// Ignore the warnings, and run ./sandbox 
+ 
 #define _GNU_SOURCE
 #include<stdio.h>
 #include<stdlib.h>
@@ -8,62 +11,111 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <pthread.h>
-
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-int play=0;
-
-void* task_thread(){
-    for(int i=0;i<10;i++){
-        //pthread_mutex_lock(&mutex);
-        while(!play){
-            pthread_cond_wait(&cond,&mutex);
-
-            pthread_mutex_unlock(&mutex);
+ 
+ 
+char* shared_memory;
+pthread_mutex_t mutex1;
+pthread_mutex_t mutex2;
+ 
+union void_cast {
+    void* ptr;
+    int value;
+};
+ 
+int VOID_TO_INT(void* ptr) {
+    union void_cast u;
+    u.ptr = ptr;
+    return u.value;
+}
+ 
+void* INT_TO_VOID(int value) {
+    union void_cast u;
+    u.value = value;
+    return u.ptr;
+}
+ 
+void* execution_function(void* argument){
+    
+    int arg = VOID_TO_INT(argument);
+    printf("Enter number of values when execution thread isn't sleeping:\n");
+    int n;
+    scanf("%d",&n);
+ 
+    for(int i=0;i<n;i++){ 
+        
+        //pthread_mutex_lock(&mutex1);
+        while(shared_memory!="Execution wake up"){
+            printf("[EXECUTION THREAD]: Locked by monitor...\n");
+            printf("Shared memory inside the while loop: %s\n",shared_memory);
+ 
+            sleep(1);
         }
-        printf("%d\n",i);
-        //sleep(1);
+          
+        printf("Shared memory after the while loop: %s\n",shared_memory);
+                //Exits while loop when flag is 1 and enters critical section.
+        printf("[EXECUTION THREAD]: Unlocked by monitor...\n");
+        
+        //Critical section
+        int x;
+        scanf("%d",&x);
+        arg += x;  
+        printf("[EXECUTION THREAD]: Executing.\n");
+        //pthread_mutex_unlock(&mutex1);
+        printf("[EXECUTION THREAD]: I unlocked myself.\n");
+           
     }
+    printf("[EXECUTION THREAD] SUM: %d\n",arg);
+    shared_memory="Die,monitor";
+    
+ 
+    //[TO BE FIXED] Putting flag to -1 will make monitor function exit
+    //the while loop, but it doesn't exit.
 }
+ 
+void* monitor_function(){
+    while(shared_memory!="Die,monitor"){
+        printf("[MONITOR THREAD]: Shared memory: %s\n",shared_memory);
+        //Just for smooth running, we put to sleep for a few seconds.
+        sleep(3);
+        printf("[MONITOR THREAD]: Locking execution thread.\n");
+        //pthread_mutex_lock(&mutex1);
+        int shmid = shmget(ftok("./",65),1024,0666|IPC_CREAT);
+        shared_memory=(char*) shmat(shmid,(void*)0,0);
 
-void* monitor_thread(){
-    //start task
-    //sleep(3)
-    //stop task
-    //sleep(3)
-    //start
-    
-    printf("[MONITOR]: Playing \n");
-    pthread_mutex_lock(&mutex);
-    play=1;
-    pthread_cond_signal(&cond);
-    pthread_mutex_unlock(&mutex);
+        
+        if(shared_memory!="Die,monitor")shared_memory="Execution go to sleep";
+        else break;
 
-    printf("[MONITOR]: Pausing \n");
-    pthread_mutex_lock(&mutex);
-    play=0;
-    pthread_mutex_unlock(&mutex);
-
-    printf("[MONITOR]: Sleeping \n");
-    sleep(3);
-
-    printf("[MONITOR]: Playing \n");
-    pthread_mutex_lock(&mutex);
-    play=1;
-    pthread_cond_signal(&cond);
-    pthread_mutex_unlock(&mutex);
-
-    
-    
+        
+        printf("Shared memory: %s\n",shared_memory);
+        sleep(3);
+        printf("[MONITOR THREAD]: Unlocking execution thread.\n");
+        //pthread_mutex_unlock(&mutex1);
+        shared_memory="Execution wake up";
+        //printf("Shared memory: %s\n",shared_memory);
+        sleep(1);
+    }
+    printf("[MONITOR THREAD] I'm done.");
 }
-
+ 
 int main(){
-    pthread_t tt,mt;
+ 
+    key_t key = ftok("./",65);
+  
+    // shmget returns an identifier in shmid
     
-    pthread_create(&mt,NULL,monitor_thread,NULL);
-    pthread_create(&tt,NULL,task_thread,NULL);
-
-    pthread_join(tt,NULL);
-    pthread_join(mt,NULL);
+ 
+    pthread_t monitor_thread;
+	pthread_t execution_thread;
     
+    long sum = 0;
+ 
+	//Concurrent execution of both threads
+	pthread_create(&monitor_thread , NULL, monitor_function,NULL);
+    pthread_create(&execution_thread , NULL, execution_function,INT_TO_VOID(sum));
+ 
+	//pthread_join waits for the threads passed as argument to finish(terminate).
+    pthread_join(execution_thread , NULL);
+    pthread_join(monitor_thread, NULL);
+    printf("\nMonitor ended.");
 }
