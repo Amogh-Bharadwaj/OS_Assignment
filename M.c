1a6772f8087b70b74c110647c39996deb581f733
Amogh-Bharadwaj <amoghrb01@gmail.com>
2021-11-15 15:25:03 +0530

    Finalised.

--- M.c
+++ M.c
@@ -8,9 +8,8 @@ int main(){
     char s[5];
     printf("Enter choice of scheduling algorithm - (fcfs (or) rr): ");
     scanf("%s",s);
-
-    printf("Reached here.");
     
+    // Running the required executable as per user input.
     if(strcmp(s,"fcfs")==0) execl("./fcfs","FCFS",NULL);
     else if(strcmp(s,"rr")==0) execl("./rr","RR",NULL);
     else printf("Invalid option.\n");
